25852898842a91707ecf74cea1e9d5ef84af5ef8
Marcie Harrison <marcie.w.harrison@gmail.com>
2019-12-05 19:10:17 -0600

    Register with eureka

--- src/main/java/com/github/marciecoder/spellbook/conjure/ConjureApplication.java
+++ src/main/java/com/github/marciecoder/spellbook/conjure/ConjureApplication.java
@@ -2,8 +2,10 @@ package com.github.marciecoder.spellbook.conjure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConjureApplication {
